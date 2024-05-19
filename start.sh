24f5789d97a8315ec1e0fbbd71b171a84c87a9b1
forval13 <149299318+forval13@users.noreply.github.com>
2024-05-19 20:53:44 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,9 +13,6 @@ chmod -R +x ./init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
 echo "done"
 echo "Creating SSL certificates"
-openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=RU/ST=MO/L=MSK/O=Org/OU=IT/CN=www.domain.ru/emailAddress=mail@domain.ru'
+openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
 echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
-echo "preparation done"
-echo "start composing..."
-docker compose up -d
-echo "Try connect https://server:8443/. The default username is guacadmin with password guacadmin."
+echo "done"
