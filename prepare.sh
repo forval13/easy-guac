6d982fb298f45ab32934d80bd9b0b8259dea88ee
forval13 <149299318+forval13@users.noreply.github.com>
2024-05-20 11:34:03 +0300

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -16,3 +16,5 @@ echo "Creating SSL certificates"
 openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
 echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
 echo "done"
+echo "If necessary, change variables in docker-compose.yml file!"
+echo "Finally, run 'docker compose up -d'".
