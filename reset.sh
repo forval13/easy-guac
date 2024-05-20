5e42cecd798db4ec38108019b3df380b5647a63c
forval13 <149299318+forval13@users.noreply.github.com>
2024-05-20 11:46:18 +0300

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-#
+# Stop all containers
 docker ps
 docker stop nginx_guacamole_compose
-docker rm nginx_guacamole_compose
 docker stop guacamole_compose
-docker rm guacamole_compose
 docker stop guacd_compose
-docker rm guacd_compose
 docker stop postgres_guacamole_compose
+docker ps
+# Remove all containers
+docker rm nginx_guacamole_compose
+docker rm guacamole_compose
+docker rm guacd_compose
 docker rm postgres_guacamole_compose
 docker ps -a
-#
+# Delete persistent data
 echo "This will delete your existing database (./data/)"
 echo "          delete your recordings        (./record/)"
 echo "          delete your drive files       (./drive/)"
