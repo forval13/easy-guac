c9c5e56c0d0b93cd3a07dd9eb55e383ee62140f1
forval13 <149299318+forval13@users.noreply.github.com>
2024-05-24 17:57:51 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -12,10 +12,12 @@ mkdir -p ./nginx/ssl >/dev/null 2>&1
 chmod -R +x ./init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > ./init/initdb.sql
 echo "done"
+echo ""
 echo "Creating SSL certificates"
 openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=RU/ST=MO/L=MSK/O=Org/OU=IT/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
 echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
 echo "done"
+echo ""
 echo "Check 'compose.yml' & '.env' files, then run 'docker compose up -d' command."
 echo "Guacamole server will be available at https://this_server_ip:8443/. The default username is 'guacadmin' with password 'guacadmin'."
 echo ""
